f8d7fdbe10a1b5219fd345328d17369244541bcd
Scott Thiessen <scott@movingobjects.io>
2025-02-02 12:33:07 -0800

    Allow deployment to Github pages

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,5 +1,9 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = { reactStrictMode: true };
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  output: 'export',
+  basePath: `/visual-electric-fireworks`,
+};
 
 export default nextConfig;
